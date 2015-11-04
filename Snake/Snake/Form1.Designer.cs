170007b4b883ab712a3ae0ce8317849d862df29b
Milos Kovacevic <miloscpt@gmail.com>
2015-11-04 01:02:02 +0100

    Last touches...

--- Snake/Snake/Form1.Designer.cs
+++ Snake/Snake/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.lblScore = new System.Windows.Forms.Label();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblGameOver = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).BeginInit();
             this.SuspendLayout();
             // 
@@ -65,16 +66,30 @@
             this.lblScore.Size = new System.Drawing.Size(0, 29);
             this.lblScore.TabIndex = 2;
             // 
+            // lblGameOver
+            // 
+            this.lblGameOver.AutoSize = true;
+            this.lblGameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameOver.Location = new System.Drawing.Point(111, 46);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(102, 37);
+            this.lblGameOver.TabIndex = 3;
+            this.lblGameOver.Text = "label2";
+            this.lblGameOver.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(650, 454);
+            this.Controls.Add(this.lblGameOver);
             this.Controls.Add(this.lblScore);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pbCanvas);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -87,6 +102,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblScore;
         private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label lblGameOver;
     }
 }
 
--- Snake/Snake/Form1.cs
+++ Snake/Snake/Form1.cs
@@ -23,7 +23,7 @@ namespace Snake
             new Settings();
 
             //setuj brzinu igre i startuj tajmer
-            gameTimer.Interval = 1000 / Settings.Speed;
+            gameTimer.Interval = 2000 / Settings.Speed;
             gameTimer.Tick += UpdateScreen;
             gameTimer.Start();
 
@@ -33,8 +33,14 @@ namespace Snake
 
         private void StartGame()
         {
+            
+            lblGameOver.Visible = false;
+
             //setovanje settings na nulu...
             new Settings();
+
+            // iz nekog razloga ovo gore ne vraca GameOver na false pa moram ovde...
+            Settings.GameOver = false;
 
             //ocisti zmiju iz predjasnje igre... i kreiraj novog player-a...
             snake.Clear();
@@ -93,6 +99,7 @@ namespace Snake
 
                 MovePlayer();
             }
+
             pbCanvas.Invalidate();
 
 
@@ -102,6 +109,132 @@ namespace Snake
         {
             Graphics canvas = e.Graphics;
 
+            if (Settings.GameOver == false)
+            {
+                // boja zmije
+                Brush snakeColour;
+
+
+                // nacrtaj zmiju
+                for (int i = 0; i < snake.Count; i++)
+                { 
+                    //crtaj glavu...
+                    if (i == 0)
+                    {
+                        snakeColour = Brushes.Black;
+                    }
+                    else
+                    {
+                        snakeColour = Brushes.Green;
+                    }
+
+                    canvas.FillEllipse(snakeColour, new Rectangle(snake[i].X * Settings.Width, 
+                                                    snake[i].Y * Settings.Height,
+                                                    Settings.Width, Settings.Height));
+                    
+                    // crtamo hranu
+                    canvas.FillEllipse(Brushes.Red, new Rectangle(food.X * Settings.Width,
+                                                    food.Y * Settings.Height, 
+                                                    Settings.Width, Settings.Height));
+                }
+            }
+            else
+            {
+                string gameOver = "Game over \n Your score is : " + Settings.Score + "\nPress enter to try again!";
+                lblGameOver.Text = gameOver;
+                lblGameOver.Visible = true;
+            }
+
+        }
+
+        private void MovePlayer()
+        {
+            for (int i = snake.Count - 1; i >= 0; i--)
+            { 
+                //move head
+                if (i == 0)
+                {
+                    switch (Settings.direction)
+                    {
+                        case Direction.Right:
+                            snake[i].X++;
+                            break;
+                        case Direction.Left:
+                            snake[i].X--;
+                            break;
+                        case Direction.Up:
+                            snake[i].Y--;
+                            break;
+                        case Direction.Down:
+                            snake[i].Y++;
+                            break;
+                    }
+
+                    // vrati max x i y poziciju...
+                    int maxXPos = pbCanvas.Width / Settings.Width;
+                    int maxYPos = pbCanvas.Height / Settings.Height;
+                    
+                    //detect collisions with game borders...
+                    if (snake[i].X < 0 || snake[i].Y < 0
+                        || snake[i].Y >= maxYPos || snake[i].X >= maxXPos)
+                    {
+                       Die();
+                    }
+
+                    //detect  self-collision with body...
+                    for (int j = 1; j < snake.Count; j++)
+                    {
+                        if (snake[i].X == snake[j].X && snake[i].Y == snake[j].Y)
+                        {
+                           Die();
+                        }
+                    }
+
+                    //detect collision with food piece...
+                    if (snake[0].X == food.X && snake[0].Y == food.Y)
+                    {
+                       Eat();
+                    }
+                    
+
+                }
+                else
+                { 
+                    //move body
+                    snake[i].X = snake[i - 1].X;
+                    snake[i].Y = snake[i - 1].Y;
+                }
+            }
+        }
+
+        private void Eat()
+        {
+            //add circle to body
+            Circle food = new Circle();
+            food.X = snake[snake.Count - 1].X;
+            food.Y = snake[snake.Count - 1].Y;
+            snake.Add(food);
+
+            //update score
+            Settings.Score += Settings.Points;
+            lblScore.Text = Settings.Score.ToString();
+
+            GenerateFood();
+        }
+
+        private void Die()
+        {
+            Settings.GameOver = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Input.ChangeState(e.KeyCode, true);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            Input.ChangeState(e.KeyCode, false);
         }
     }
 }
